b67a97b8c79705da9f0610ba1cf0c8aa82fe179e
adamrd231 <adam.rdconcepts@gmail.com>
2022-03-26 13:24:07 -0600

    Initial Commit

---
+++ hBowl/hBowlApp.swift
@@ -0,0 +1,17 @@
+//
+//  hBowlApp.swift
+//  hBowl
+//
+//  Created by Adam Reed on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct hBowlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
